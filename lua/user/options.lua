11e49eb9421e7bb43e0dd7e5642d9d466c8862a9
Laris Qiao <laris.qiao@gmail.com>
2023-11-18 12:02:44 +0800

    personality, termguicolors true, relativenumber true, numberwidth 2, list and listchars

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -16,7 +16,7 @@ local options = {
   splitbelow = true,                       -- force all horizontal splits to go below current window
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
-  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
+  termguicolors = true,                    -- set term gui colors (most terminals support this)
   timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
@@ -26,8 +26,8 @@ local options = {
   tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
-  relativenumber = false,                  -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  relativenumber = true,                  -- set relative numbered lines
+  numberwidth = 2,                         -- set number column width to 2 {default 4}
 
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = true,                             -- display lines as one long line
@@ -36,6 +36,8 @@ local options = {
   sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
   whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
+  list = true,                             -- show unvisible char
+  listchars = "tab:→␣,space:·,nbsp:☠,trail:•,eol:¶,precedes:«,extends:»,"
 }
 
 for k, v in pairs(options) do
