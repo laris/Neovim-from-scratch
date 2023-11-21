d188da389bbffaa1b27f11f6ea0d33e7a3cf3fe1
Laris Qiao <laris.qiao@gmail.com>
2023-11-21 22:42:38 +0800

    fix filetype checking, works great now

--- lua/user/cargoplay.lua
+++ lua/user/cargoplay.lua
@@ -37,33 +37,93 @@ function is_rust_file() -- bug, don't know why return empty value
     error("Current open filetype: " .. filetype)
   end
 end
--- check cargo play firstly, then check file type
-local st_cargo, msg_cargo = pcall(check_cargo_play)
---local st_rust_file, msg_rust_file = pcall(is_rust_file)
-local st_rust_file, msg_rust_file = true, "fake rust true"
---vim.api.nvim_out_write("pcall: st_rust_file=" ..  tostring(st_rust_file) .. "\n" .. msg_rust_file .. "\n")
-if st_cargo and st_rust_file then
-  -- if cargo play works, set CargoPlay as com
-  --vim.api.nvim_out_write(msg .. "\n")
-  vim.cmd("command! CargoPlay !cargo play %")
-else
-  vim.api.nvim_out_write("Error: " .. msg_cargo .. msg_rust_file .. "\n")
-  return
-end
+-- -- check cargo play firstly, then check file type
+-- local st_cargo, msg_cargo = pcall(check_cargo_play)
+-- local st_rust_file, msg_rust_file = pcall(is_rust_file)
+-- --local st_rust_file, msg_rust_file = true, "fake rust true"
+-- --vim.api.nvim_out_write("pcall: st_rust_file=" ..  tostring(st_rust_file) .. "\n" .. msg_rust_file .. "\n")
+-- if st_cargo and st_rust_file then
+--   -- if cargo play works, set CargoPlay as com
+--   --vim.api.nvim_out_write(msg .. "\n")
+--   vim.cmd("command! CargoPlay !cargo play %")
+-- else
+--   vim.api.nvim_out_write("Error: " .. msg_cargo .. msg_rust_file .. "\n")
+--   return
+-- end
 
 --vim.cmd([[ command! CargoPlay lua require('mycommands').cargo_play() ]])
 
 --M = {}
 ----M.cargo_play()
 --function M.cargo_play()
---  local current_file = vim.fn.expand('%:p')
---  local command = string.format('cargo play %s', current_file)
---  vim.api.nvim_out_write("debug: "..current_file.."--"..command.."\n")
---  vim.fn.system(command)
---  return "cargo_play"
---end
+function cargo_play()
+  local current_file = vim.fn.expand('%:p')
+  local command = string.format('cargo play %s', current_file)
+  --vim.api.nvim_out_write("debug: "..current_file.."--"..command.."\n")
+  --vim.fn.system(command)
+  --vim.cmd("silent execute '!" .. command .. "'")
+  --vim.cmd("silent! read ! " .. command)
+  -- Run the cargo play command and capture the output in a list
+  local output = vim.fn.systemlist(command)
+--------------------------------------------------------------------------------
+  -- Display the output in the bottom minibuffer
+  for _, line in ipairs(output) do
+    vim.api.nvim_out_write(line .. "\n")
+  end
+--------------------------------------------------------------------------------
+--    -- Create a new buffer for displaying the output
+--  local output_buffer = vim.api.nvim_create_buf(false, true)
+--  -- Set the output buffer as the current buffer
+--  vim.api.nvim_set_current_buf(output_buffer)
+--  -- Display the output in the new buffer
+--  for _, line in ipairs(output) do
+--    vim.api.nvim_buf_set_lines(output_buffer, -1, -1, false, {line})
+--  end
+--  -- Open a new split window for the output buffer below the current window
+--  vim.cmd('split')
+--------------------------------------------------------------------------------
+--  -- Save the current window and cursor position
+--  local original_window = vim.fn.winnr()
+--  local original_cursor = vim.fn.getpos('.')
+--
+--  -- Run the cargo play command and capture the output in a list
+--  local output = vim.fn.systemlist(command)
+--
+--  -- Create a new buffer for displaying the output
+--  local output_buffer = vim.api.nvim_create_buf(false, true)
+--
+--  -- Set the output buffer as the current buffer
+--  vim.api.nvim_set_current_buf(output_buffer)
+--
+--  -- Display the output in the new buffer
+--  for _, line in ipairs(output) do
+--    vim.api.nvim_buf_set_lines(output_buffer, -1, -1, false, {line})
+--  end
+--
+--  -- Open a new split window for the output buffer below the current window
+--  vim.cmd('split')
+--
+--  -- Restore the original window and cursor position
+--  vim.fn.win_gotoid(original_window)
+--  vim.fn.setpos('.', original_cursor)
+ --------------------------------------------------------------------------------
+end
 --return M
 
+function checked_cargo_play()
+  local status_cargo, msg_cargo = pcall(check_cargo_play)
+  local status_rust_file, msg_rust_file = pcall(is_rust_file)
+  if status_cargo and status_rust_file then
+    cargo_play()
+    return
+  else
+    vim.api.nvim_out_write("Error: need both `cargo play` and `rust` file:\ncargo play status: " .. msg_cargo .."\nCurrent file type: ".. msg_rust_file .. "\n")
+    return
+  end
+end
+
+--vim.cmd("command! -nargs=0 CargoPlay call s:checked_cargo_play()")
+vim.cmd("command! -nargs=0 CargoPlay lua checked_cargo_play()")
 --local keymap = vim.api.nvim_set_keymap
 --local opts = { noremap = true, silent = true }
 
